1b5fb1de4901661d925f1eb543552953d4e9443d
bakterimu <izhar.mayranda@gmail.com>
2024-06-16 21:03:15 +0700

    add test type data

---
+++ test/type-data.test.ts
@@ -0,0 +1,14 @@
+describe("Test type data", () => {
+    it("should success", () => {
+        let name = "budi";
+        let nomor = 10000;
+        let isVip = true;
+
+        console.log(name);
+        console.log(nomor);
+        console.log(isVip);
+
+        // name = 100;
+        // nomor = "sera";
+    });
+});
