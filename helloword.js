3850b3ea6bdb2a8ccc143e20a0d74ad835bbeb77
bakterimu <izhar.mayranda@gmail.com>
2024-06-16 13:53:39 +0700

    first commit

---
+++ helloword.js
@@ -0,0 +1,5 @@
+var message = 'Hellowwww';
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+console.log(message);
---
+++ helloword.ts
@@ -0,0 +1,5 @@
+let message: string = 'Hellowwww';
+let heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+console.log(message);
